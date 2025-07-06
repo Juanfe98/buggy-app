b6fbfbdac68ef719153e902dfbe5f711111045da
Juan Felipe Montana Perdomo <juanmontana1398@gmail.com>
2025-07-06 16:54:56 -0500

    Return also palette from theme hook

--- hooks/useTheme.tsx
+++ hooks/useTheme.tsx
@@ -1,12 +1,19 @@
-import { useColorScheme } from 'react-native'
-import { Colors, ThemeColors } from '@/constants/Colors'
+import { useColorScheme } from 'react-native';
+import { Colors, palette, ThemeColors } from '@/constants/Colors';
 
+interface AppColors {
+  theme: ThemeColors;
+  palette: string[];
+}
 /**
- * A simple hook that returns the correct color palette 
+ * A simple hook that returns the correct color palette
  * (light or dark) based on the OS setting.
  */
-export default function useTheme(): ThemeColors {
-  const colorScheme = useColorScheme() // 'light' | 'dark' | null
+export default function useTheme(): AppColors {
+  const colorScheme = useColorScheme(); // 'light' | 'dark' | null
   // Fallback to 'light' if `useColorScheme()` returns null (e.g. older Android)
-  return colorScheme === 'dark' ? Colors.dark : Colors.light
+  return {
+    theme: colorScheme === 'dark' ? Colors.dark : Colors.light,
+    palette: palette,
+  };
 }
